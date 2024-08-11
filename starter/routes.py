b25bde475f0c288d13129ca289bf120cf9a8931e
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-11 22:41:51 +0300

    logout added

--- starter/routes.py
+++ starter/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from starter import app, bcrypt, db
 from starter.form import RegistrationForm, LoginForm
 from starter.models import User, Post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -63,3 +63,8 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home')) 
