2ec5aa151e621d81b4336a040d7c1adc6cce59fe
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-15 17:10:54 +0300

    post route created

--- starter/routes.py
+++ starter/routes.py
@@ -102,3 +102,10 @@ def new_post():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('post.html', title=post.title, post=post)
