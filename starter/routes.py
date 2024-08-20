5954478cb39fb90a08501bc2d2f783f0d8eb01bd
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-20 23:39:50 +0300

    reset route initiated

--- starter/routes.py
+++ starter/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from starter import app, bcrypt, db
-from starter.form import RegistrationForm, LoginForm, UpdadteAccountForm, PostForm
+from starter.form import RegistrationForm, LoginForm, UpdadteAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from starter.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
