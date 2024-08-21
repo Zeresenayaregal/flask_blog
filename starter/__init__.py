65b5835dd3a17656fee59198e514b14dde2c377f
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-21 09:52:52 +0300

    SMTP added

--- starter/__init__.py
+++ starter/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2fe0b2a9ab8d2be9750e06eb8a11df20'
@@ -11,5 +12,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.google.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 
 from starter import routes
