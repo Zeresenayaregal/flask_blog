b444448861da9d4275e249ec55bf5257b7710a33
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-21 09:52:36 +0300

    routes created for reset request

--- starter/routes.py
+++ starter/routes.py
@@ -3,9 +3,10 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from starter import app, bcrypt, db
-from starter.form import RegistrationForm, LoginForm, UpdadteAccountForm, PostForm, RequestResetForm, ResetPasswordForm
+from starter.form import (RegistrationForm, LoginForm, UpdadteAccountForm, 
+                          PostForm, RequestResetForm, ResetPasswordForm)
 from starter.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, current_user, logout_user, login_required # type: ignore
 
 
 
@@ -153,4 +154,32 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts, user=user)
+
+
+def send_reset_email(user):
+    pass
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
 
