261425b3d994cc7c902e959dd120a2a5a59a6d0e
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-08-29 23:11:18 +0300

    app instance

--- starter/__init__.py
+++ starter/__init__.py
@@ -5,19 +5,31 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from starter.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+mail = Mail()
 
-from starter.users.routes import users
-from starter.posts.routes import posts
-from starter.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from starter.users.routes import users
+    from starter.posts.routes import posts
+    from starter.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
